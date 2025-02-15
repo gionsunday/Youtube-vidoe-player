5fa67f11189f2bb7c7d32fe4bb4fdb70c07fecdc
gionsunday <richardadaji@gmail.com>
2025-02-15 21:07:44 +0100

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Build and Deploy a Modern YouTube Clone Application in React JS with Material UI 5
+# Run npm i or npm install
+# Get api key from rapidapi at https://rapidapi.com/ytdlfree/api/youtube-v31
+# Run  npm start
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'x-rapidapi-key': '6310c2ad85msh212b8a229e73164p1cda95jsn59a4d2c64d58',
+    'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
